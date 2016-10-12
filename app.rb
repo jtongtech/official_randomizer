db5705b0f02c8515a61d021497b77d413f37b579
JTong <eldertong@gmail.com>
2016-10-12 11:49:16 -0400

    what happens to the text inputed into the java boxes

--- app.rb
+++ app.rb
@@ -7,5 +7,18 @@ get '/' do
     erb :number_students1
 end
 
-# document.createElement("INPUT");
-# x.setAttribute("type", "text");
+post '/number_of_students' do
+    quantity = params[:quantity]
+    erb :get_names, :locals => {:quantity => quantity}
+end
+
+get '/get_names?' do
+    quantity = params[:quantity]
+    erb :get_names, :locals => {:quantity => quantity}
+end
+
+post '/names' do
+    names = params[:user_names]
+    results = randomizer(names)
+    "results are #{results}"
+end
